8f9a5693bd1b46c3c4189e20efd6bc4c7af8482c
Shafiqah <shafiqah.rostam@hotmail.sg>
2024-05-10 00:25:57 -0400

    Added components and functionalities as per lab instructions

--- react-redux-master/src/index.js
+++ react-redux-master/src/index.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';  // Updated import
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import myReducers from './reducers';
+import { legacy_createStore as createStore } from 'redux';
 
-import {createStore} from 'redux';
-import {Provider} from 'react-redux'
-import myReducers from './reducers'
-
-
-//Create the store
+// Create the store
 const myStore = createStore(myReducers);
 
-//This will console log the current state everytime the state changes
-myStore.subscribe(()=>console.log(myStore.getState()));
+// This will console log the current state every time the state changes
+myStore.subscribe(() => console.log(myStore.getState()));
 
-//Enveloping the App inside the Provider, ensures that the states in the store are available
-//throughout the application
-ReactDOM.render(<Provider store={myStore}><App/></Provider>, document.getElementById('root'));
+// Enveloping the App inside the Provider, ensures that the states in the store are available
+// throughout the application
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// As per React 18, using createRoot
+const root = createRoot(document.getElementById('root'));
+root.render(<Provider store={myStore}><App /></Provider>);
